23e5b196c55ada21d3a8df532ca19041b6f427b3
kjj6198 <kjj6198@gmail.com>
2020-03-10 00:49:20 +0900

    added variable basic

---
+++ basic.pm
@@ -0,0 +1,18 @@
+# hello world!
+print "Hello, world\n";
+
+# difference between '' and ""
+print "Hello world\n";
+print 'hello world\n';
+print "\n";
+
+# variable
+
+$a = 10;
+print($a);
+
+# interpolation
+
+$a = "who are you?";
+
+print("$a I'm Kalan\n");
