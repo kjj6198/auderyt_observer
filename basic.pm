bea1d9a9eb6835c98d4661e4500252e1b92a63bb
kjj6198 <kjj6198@gmail.com>
2020-03-10 00:56:48 +0900

    Add hash and qw

--- basic.pm
+++ basic.pm
@@ -22,4 +22,29 @@ print("$a I'm Kalan\n");
 @age = (25, 30, 40);
 
 # what difference between @age[0] and $age[0] 
-print("@age[0]");
+print("@age[0]\n");
+
+%data = (
+  'kalan', 25, 'john', 30
+);
+
+print "$data{'john'}\n";
+
+# qw
+
+@days = qw/Mon Tue Wed Thu Fri Sat Sun/;
+@weekdays = @days[3..5];
+
+print "@weekdays\n";
+
+# hash
+
+%data2 = (
+  'name' => 'Kalan',
+  'age' => 20,
+);
+
+print "$data2{'name'}\n";
+
+@names = keys %data2;
+print "$names[0]\n";
